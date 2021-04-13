0374cd0e0115ebad9bf1c4ed22e8be567ac4597e
z5116011-Otto <z5116011@student.unsw.edu.au>
2021-04-13 19:18:07 +1000

    giving prometheus volume

--- metricsPrometheus.sh
+++ metricsPrometheus.sh
@@ -3,6 +3,6 @@
 sudo docker run -d --name prometheus-server -p 9090:9090 \
   --network pi-network \
   --restart always \
-  -v /prometheus.yml:/prometheus.yml \
+  -v ./prometheus.yml:/prometheus.yml \
   prom/prometheus \
   --config.file=/prometheus.yml

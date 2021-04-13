abad90c6b4240f651abb7444f65a13b9ec00dbe2
Otto <z5116011@student.unsw.edu.au>
2021-04-13 19:31:41 +1000

    Prometheus works

--- metricsPrometheus.sh
+++ metricsPrometheus.sh
@@ -3,6 +3,6 @@
 sudo docker run -d --name prometheus-server -p 9090:9090 \
   --network pi-network \
   --restart always \
-  -v ./prometheus.yml:/prometheus.yml \
+  -v /home/otto/Desktop/Thesis/HLF-for-RPI-/prometheus.yml:/prometheus.yml \
   prom/prometheus \
   --config.file=/prometheus.yml

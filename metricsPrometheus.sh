1221f75435e89f837a1cbfd062a7444b07dad993
z5116011-Otto <z5116011@student.unsw.edu.au>
2021-04-13 19:02:18 +1000

    adding promethues

---
+++ metricsPrometheus.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo docker run -d --name prometheus-server -p 9090:9090 \
+  --network pi-network \
+  --restart always \
+  -v /prometheus.yml:/prometheus.yml \
+  prom/prometheus \
+  --config.file=/prometheus.yml
